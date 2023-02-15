0db2ed2895831b00e05d5037cda926aae46c3f39
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-16 05:59:41 +0900

    Googleログイン

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,19 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
 
 const Login = () => {
-  return <div>Login</div>;
+  const loginInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result);
+    });
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
