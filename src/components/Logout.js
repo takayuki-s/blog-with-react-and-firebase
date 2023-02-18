c27ef3e93ba458fa60afd87cba52ac2a69aaba09
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-18 21:25:07 +0900

    ログアウト（エラー）

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,23 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase';
 
-const Logout = () => {
-  return <div>Logout</div>;
+const Logout = ({ setIsAuth }) => {
+  const navigate = useNavigate;
+  const logout = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/');
+    });
+  };
+  return (
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
+  );
 };
 
 export default Logout;
