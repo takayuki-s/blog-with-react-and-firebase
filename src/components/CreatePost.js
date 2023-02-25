83535e41e574f795f5eea628e7b14306e912e4d6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-25 21:32:50 +0900

    記事投稿ページ文字列取得

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreatePost.css';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+
+  const createPost = () => {
+    console.log(title);
+    console.log(postText);
+  };
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入" />
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>投稿</div>
-          <textarea placeholder="投稿内容をを記入" />
+          <textarea
+            placeholder="投稿内容をを記入"
+            onChange={(e) => setPostText(e.target.value)}
+          />
         </div>
-        <button className="postButton">投稿する</button>
+        <button className="postButton" onClick={createPost}>
+          投稿する
+        </button>
       </div>
     </div>
   );
