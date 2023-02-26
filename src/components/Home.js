ecd31c39d587c82a42fcc1736299b2d969b6b7c8
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-26 10:17:44 +0900

    HomeコンポーネントにCSS適用

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
@@ -7,13 +8,13 @@ const Home = () => {
         <div className="postHeader">
           <h1>タイトル</h1>
         </div>
-      </div>
-      <div className="postTextContainer">
-        今はReactの学習中です。これからReactのエンジニアとして頑張っていきます。よろしくお願いいたします。
-      </div>
-      <div className="nameAndDeleteButton">
-        <h3>@teke</h3>
-        <button>削除</button>
+        <div className="postTextContainer">
+          今はReactの学習中です。これからReactのエンジニアとして頑張っていきます。よろしくお願いいたします。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@teke</h3>
+          <button>削除</button>
+        </div>
       </div>
     </div>
   );
