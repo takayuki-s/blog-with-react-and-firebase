77ea3116727520023b3c469425f74e249129baf2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-27 08:41:14 +0900

    ブログデータを１つずつ取り出して表示（エラー）
    ncaught TypeError: Cannot read properties of undefined (reading 'map')

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,36 @@
 import { collection, getDocs } from 'firebase/firestore';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import './Home.css';
 
 const Home = () => {
+  const [postList, setPostList] = useState();
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, 'posts'));
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map((doc) => ({ ...doc.data() })));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ ...doc.data() })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data() })));
     };
     getPosts();
   }, []);
   return (
     <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents" kye={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
       <div className="postContents">
         <div className="postHeader">
           <h1>タイトル</h1>
